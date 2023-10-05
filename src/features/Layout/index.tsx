d1720fe3f77c9d1e1af183b7230999a759e74e22
arubua <rubua31@gmail.com>
2023-10-05 10:36:15 +0300

    created storage utilities

--- src/features/Layout/index.tsx
+++ src/features/Layout/index.tsx
@@ -1,9 +1,114 @@
-import React from 'react'
+// import React, { useEffect, useState } from "react";
+// import { useNavigate, useLocation } from "react-router-dom";
+// // import { Sidebar } from "../Sidebar";
+// // import { Navbar } from "../Navbar";
+// // import { clearStorage, getStorage } from "../../utils/storage";
+// // import appTitle from "../../utils/appTitle";
+// // import userLoggedInTimeout from "../../utils/userLoggedInTimeout";
+// // import { AUTH_END_POINT, NTSATEST_END_POINT } from "../../constants/strings";
+// // import axios from "axios";
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
+// interface LayoutProps {
+//   app: any; 
+//   menus: any[]; 
+//   user: any; 
+//   children: React.ReactNode;
+// }
 
-export default index
+// const Layout: React.FC<LayoutProps> = (props) => {
+//   const [title, setTitle] = useState<string>("");
+//   const [user, setUser] = useState<any>();
+//   const [isSidebarFullVisible, setSideBarVisibility] = useState<boolean>(true);
+//   const [alerts, setAlerts] = useState<any[]>([]);
+//   const [isLoadingViolationNotification, setIsLoading] = useState<boolean>(true);
+
+//   const navigate = useNavigate();
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     const init = async () => {
+//       // await setUserLoggedIn();
+//       // await getAccount(user.token);
+//     };
+//     init();
+//   }, []);
+
+//   useEffect(() => {
+//     const pathname = location.pathname;
+//     let path = pathname.replaceAll("/", " ").trim();
+//     // document.title = appTitle(path);
+//     setTitle(path);
+//   }, [location]);
+
+//   const setUserLoggedIn = async () => {
+//     let user = await getStorage();
+//     if (!user.token && !user.accountVerified) return navigate("/login");
+//     const isSessionExpired = userLoggedInTimeout(user);
+//     if (isSessionExpired) {
+//       return navigate("/auth/signin");
+//     }
+//     setUser(user);
+//   };
+
+//   function onToggle() {
+//     const sidebar_class = document.getElementById("sidebar")!.classList;
+//     if (sidebar_class.contains("show")) {
+//       sidebar_class.remove("show");
+//       sidebar_class.add("hide");
+//       return;
+//     }
+//     sidebar_class.add("show");
+//     sidebar_class.remove("hide");
+//   }
+
+//   function hideSidebarOnMobile() {
+//     const screenWidth = window.screen.availWidth;
+//     const sidebar_class = document.getElementById("sidebar")!.classList;
+//     if (screenWidth < 600 && sidebar_class.contains("show")) {
+//       sidebar_class.remove("show");
+//       sidebar_class.add("hide");
+//       return;
+//     }
+//   }
+
+//   const onSignMeOut = async () => {
+//     await clearStorage();
+//     navigate("/auth/signin");
+//   };
+
+//   return (
+//     <div>
+//       <div className="app-home-container">
+//         <Sidebar
+//           onToggle={onToggle}
+//           app={props.app}
+//           menus={props.menus || []}
+//           pathname={location.pathname}
+//           hideSidebarOnMobile={hideSidebarOnMobile}
+//         />
+//         <div className="app-main-container">
+//           <Navbar
+//             user={props.user || {}}
+//             onToggle={onToggle}
+//             onSignMeOut={onSignMeOut}
+//             title={title}
+//             notifications={alerts}
+//             isLoadingViolationNotification={isLoadingViolationNotification}
+//           />
+//           <div id="app-main-content" className="app-main-content">
+//             <div
+//               style={{
+//                 paddingLeft: "1rem",
+//                 paddingRight: "1rem",
+//               }}
+//             >
+//               {props.children}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Layout;
---
+++ src/hooks/hooks.ts
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+
+export function useSessionStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T) => void] {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = sessionStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(`Error retrieving sessionStorage key "${key}": ${error}`);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T) => {
+    try {
+      setStoredValue(value);
+      sessionStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(`Error setting sessionStorage key "${key}": ${error}`);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T) => void] {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(`Error retrieving localStorage key "${key}": ${error}`);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T) => {
+    try {
+      setStoredValue(value);
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(`Error setting localStorage key "${key}": ${error}`);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+
